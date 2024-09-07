c9697df4541f5376580541a22854f529c4ca2964
AdityaT-19 <beanadi1910@gmail.com>
2024-09-07 15:57:13 +0530

    Initial Setup with Basic functionality

---
+++ src/main.rs
@@ -0,0 +1,39 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(name = "RustyGo🚀")]
+#[command(version = "0.0.1")]
+#[command(about = "A Rust based auto-reloader for Go! 🚀", long_about = None)]
+#[command(next_line_help = true)]
+#[command(propagate_version = true)]
+struct Cli {
+    /// Enable htmx support
+    #[arg(short = 'x', long)]
+    htmx: bool,
+    #[command(subcommand)]
+    command: Commands,
+}
+#[derive(Subcommand)]
+enum Commands {
+    /// Run the Go server
+    Run,
+    /// Initialize a config file
+    Init,
+}
+fn main() {
+    let cli = Cli::parse();
+    if cli.htmx {
+        println!("Htmx support enabled");
+    } else {
+        println!("Htmx support disabled");
+    }
+
+    match cli.command {
+        Commands::Run => {
+            println!("Running the Go server");
+        }
+        Commands::Init => {
+            println!("Initializing the config file");
+        }
+    }
+}
